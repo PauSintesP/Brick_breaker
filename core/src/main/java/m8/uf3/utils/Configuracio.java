53a58694d4cbe1048b94506cf3086f4aa4d7c0d4
Yeray <2023_yeray.reyes@iticbcn.cat>
2025-04-29 18:59:02 +0200

    update config file

--- core/src/main/java/m8/uf3/utils/Configuracio.java
+++ core/src/main/java/m8/uf3/utils/Configuracio.java
@@ -1,10 +1,12 @@
 package m8.uf3.utils;
 
 public class Configuracio {
+    // Per a la Barra
+    public static final float VELOCITAT_BARRA = 300;
+    public static final float AMPLADA_BARRA = 128;
+    public static final float ALTURA_BARRA = 32;
+
+    // Mides del joc
     public static final float AMPLADA_JO = 480;
     public static final float ALTURA_JO = 800;
-    public static final float AMPLADA_PALETA = 100;
-    public static final float ALTURA_PALETA = 20;
-    public static final float VELOCITAT_PALETA = 300;
-    public static final float ANGLE_MAXIM_PILOTA = 150;
 }
