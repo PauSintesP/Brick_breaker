8fc77f2b0d70f1ab8c3ff4190a0339498e44d08c
Yeray <2023_yeray.reyes@iticbcn.cat>
2025-04-29 18:51:52 +0200

    create Configuacio file and add short app settings

---
+++ core/src/main/java/m8/uf3/utils/Configuracio.java
@@ -0,0 +1,10 @@
+package m8.uf3.utils;
+
+public class Configuracio {
+    public static final float AMPLADA_JO = 480;
+    public static final float ALTURA_JO = 800;
+    public static final float AMPLADA_PALETA = 100;
+    public static final float ALTURA_PALETA = 20;
+    public static final float VELOCITAT_PALETA = 300;
+    public static final float ANGLE_MAXIM_PILOTA = 150;
+}
